c6852700a06eefca623f08f33861ca7213ba3a11
MasterShifu <136521147+MasterShifuCsgo@users.noreply.github.com>
2023-11-24 20:08:17 +0200

    09-Data-Structures-Operators/starter/script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,16 +1,17 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
+
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   openingHours: {
     thu: {
@@ -26,4 +27,147 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery(starterIndex = 1, mainIndex = 0, time = "20:00", address) {
+    console.log(
+      `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicous pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
+
+//spread, on the right side of =
+const arr = [1, 2, ...[3, 4]];
+
+//Rest, because on left side on =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+
+/*
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(bardNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr); // logs individual elements
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu); // all elements + gnocci
+
+// Copy array
+
+const mainMenuCopy = [...restaurant.mainMenu];
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+//It Iterables: arrays, strings, maps, sets, NOT objects
+const str = "Jonas";
+const letters = [...str, " ", "s."];
+console.log(letters); //each letter is now an individual element
+console.log(...str);
+//console.log(`${...str}: Schmedtmann`); //${} does not take multiple values
+//Real world example
+const ingredients = [
+  prompt("Let's make pasta! ingredient 1?"),
+  prompt("Let's make pasta! ingredient 2?"),
+  prompt("Let's make pasta! ingredient 3?"),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]); // old way
+
+restaurant.orderPast(...ingredients); // modern way
+
+//Objects
+const newRestautant = { foundedIn: 1998, ...restaurant, founder: "Guiseppe" };
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+//copy of the obj
+//END
+
+/*
+restaurant.orderDelivery({
+  time: "22:30",
+  addess: "Vie del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: "Via del Sole, 21",
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+//default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+let a = 111;
+let b = 999;
+
+const obj = { a: 23, b: 7, c: 14 };
+// overwrite a and b
+({ a, b } = obj);
+
+// nestd objs
+
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(open, close, o, c);
+
+/*
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+
+console.log(x, y, z);
+console.log(arr);
+
+const [main, , secondary] = restaurant.categories;
+
+console.log(main, secondary);
+
+const temp = main;
+main = secondary;
+secondary = temp;
+
+[main, secondary] = [secondary, main];
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+
+const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+
+console.log(p, q, r); // last element is undefined if default value is not set.
+
+*/
